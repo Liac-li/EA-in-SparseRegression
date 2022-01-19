dd2ad5265deb395fa97caecda047273c4596d31b
leelin <lyt-42@outlook.com>
2022-01-20 06:15:57 +0800

    Initialize Reposity

---
+++ utils.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+import pandas
+import numpy as np
+
+
+class DataLoader(object):
+    def __init__(self, data_folder):
+        self.path = data_folder
+        
+        print("TODO")
+    
+
